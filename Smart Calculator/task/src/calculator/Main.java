202b6c6156f9ee438cf3104e165e46dc36807fc0
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-12-18 17:49:54 +0200

    0003 stage 3/8 done

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -3,14 +3,37 @@ package calculator;
 import java.util.Scanner;
 
 public class Main {
-    private static final String EXIT = "/exit";
+    private static final String EXIT_COMMAND = "/exit";
+    private static final String HELP_COMMAND = "/help";
+    private static final String HELP_MESSAGE = "The program calculates the sum of numbers";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
         do {
             input = scanner.nextLine();
-            if (input.equals(EXIT)) {
+            if (input.equals(EXIT_COMMAND)) {
+                break;
+            }
+            if (input.isEmpty()) {
+                continue;
+            }
+            if (input.equals(HELP_COMMAND)) {
+                System.out.println(HELP_MESSAGE);
+                continue;
+            }
+            System.out.println(calculateSum(input));
+        } while (true);
+
+        System.out.println("Bye!");
+    }
+
+    public static void stageTwo() {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        do {
+            input = scanner.nextLine();
+            if (input.equals(EXIT_COMMAND)) {
                 break;
             }
             if (input.isEmpty()) {
@@ -26,5 +49,15 @@ public class Main {
         } while (true);
 
         System.out.println("Bye!");
+
+    }
+
+    public static int calculateSum(String input) {
+        String[] operands = input.split("\\s+");
+        int sum = 0;
+        for (int i = 0; i < operands.length; i++) {
+            sum += Integer.parseInt(operands[i]);
+        }
+        return sum;
     }
 }
